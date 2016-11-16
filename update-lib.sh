ba82508ec932917aca7c2c637140646ddbffe304
Josh Glendenning <joshglendenning@gmail.com>
2016-11-16 00:57:42 -0600

    Update nats.js, clean up everything

--- update-lib.sh
+++ update-lib.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 vimdiff lib/nats.js <(curl 'https://raw.githubusercontent.com/nats-io/node-nats/master/lib/nats.js')
-vimdiff lib/nuid.js <(curl 'https://raw.githubusercontent.com/nats-io/node-nats/master/lib/nuid.js')
+vimdiff lib/nuid.js <(curl 'https://raw.githubusercontent.com/nats-io/node-nuid/master/lib/nuid.js')
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
 var webpack = require('webpack');
 module.exports = {
-	entry:  './bootstrap.js',
-	output: {
-		path:     './dist',
-		filename: 'bundle.js',
-	},
-	plugins: [
-		new webpack.optimize.UglifyJsPlugin({minimize: true})
-	]
+  entry:  './bootstrap.js',
+  output: {
+    path: './dist',
+    filename: 'bundle.js',
+  }
 };
+
+if (process.env.NODE_ENV === 'prod') {
+  module.exports.plugins = (module.exports.plugins || []).concat([
+		new webpack.optimize.UglifyJsPlugin({minimize: true})
+  ]);
+}
